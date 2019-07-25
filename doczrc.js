8cfe3b92bf36ba95e7ead5d29ce74f68e680c5ec
Lando Schmuck <lando@Landos-MacBook-Pro.local>
2019-07-25 15:02:01 +0200

    Add dependencies

---
+++ doczrc.js
@@ -0,0 +1,17 @@
+export default {
+  htmlContext: {
+    head: {
+      links: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css?family=Roboto&display=swap"
+        },
+        {
+          rel: "stylesheet",
+          href:
+            "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css"
+        }
+      ]
+    }
+  }
+};
