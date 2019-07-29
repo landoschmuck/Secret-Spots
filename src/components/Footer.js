aab2fc7b93d4778edabfc5904797892e53f86206
Lando Schmuck <lando@Landos-MacBook-Pro.local>
2019-07-29 15:13:01 +0200

    Add Font Aw

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import NavButton from "./components/NavButton";
 
 const Footer = styled.footer`
   height: 80px;
