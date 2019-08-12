a46a91c00aef92b916c009b4e0ebc4bab02176f4
Lando Schmuck <lando@Landos-MacBook-Pro.local>
2019-08-12 15:59:48 +0200

    new header/footer color

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -1,16 +1,24 @@
 import React from "react";
 import Map from "../components/Map";
+import styled from "styled-components";
+import { fadePage } from "../utils/animations";
+
+const FadeContainer = styled.div`
+  animation: ${fadePage} 0.5s ease both;
+`;
 
 function Overview({ width, height, spots, center, onSetLocation }) {
   return (
-    <Map
-      spots={spots}
-      center={center}
-      zoom={3}
-      height={height}
-      width={width}
-      onMapClick={onSetLocation}
-    />
+    <FadeContainer>
+      <Map
+        spots={spots}
+        center={center}
+        zoom={3}
+        height={height}
+        width={width}
+        onMapClick={onSetLocation}
+      />
+    </FadeContainer>
   );
 }
 
