f40ffe0a5a47d655d1d5ff9917a18fe9612231b1
Lando Schmuck <lando@Landos-MacBook-Pro.local>
2019-08-23 12:15:30 +0200

    fix server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const fs = require("fs");
 const api = require("./server-api");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
@@ -25,7 +26,6 @@ mongoose
   })
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error(err));
-
 // static file serving
 app.use(express.static(path.join(__dirname, "build")));
 
